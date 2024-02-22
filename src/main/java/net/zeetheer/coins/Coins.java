54ec093daa0c218c7b7ac18143189f247f7193cc
Snaplan <81860686+Snaplan@users.noreply.github.com>
2024-02-22 20:09:11 +0900

    Iron Coin

--- src/main/java/net/zeetheer/coins/Coins.java
+++ src/main/java/net/zeetheer/coins/Coins.java
@@ -2,6 +2,8 @@ package net.zeetheer.coins;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.zeetheer.coins.item.ModItemGroups;
+import net.zeetheer.coins.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class Coins implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
